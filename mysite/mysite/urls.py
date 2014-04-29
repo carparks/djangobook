6aa75607d96b9cb4cc99a546d51c9ed186316f12
Mike Garcia <mikegarcia@texas811.org>
2014-04-29 16:12:45 -0500

    Django Chapter 3 Changes

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import patterns, include, url
+from mysite.views import hello, current_datetime, hours_ahead
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+	url(r'^hello/$', hello),
+	url(r'^time/$', current_datetime),
+	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
