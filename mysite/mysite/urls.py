5513fa0ac7d638bfc5c34b7af62e29d6e3f44457
Mike Garcia <mikegarcia@texas811.org>
2014-05-08 18:34:23 -0500

    Chapter 7 Searches

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from mysite.views import hello, current_datetime, hours_ahead, display_meta
+from books import views
 
 from django.contrib import admin
 admin.autodiscover()
@@ -9,6 +10,8 @@ urlpatterns = patterns('',
 	url(r'^time/$', current_datetime),
 	url(r'^time/plus/(\d{1,2})/$', hours_ahead),
 	url(r'^meta/$', display_meta),
+	url(r'^search-form/$', views.search_form),
+	url(r'^search/$', views.search),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
