9f1108166708f9d327ef1194f82224e1d2c7d0ff
Mike Garcia <mikegarcia@texas811.org>
2014-05-14 22:12:45 -0500

    More Ch 7 Forms

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, HttpResponse
 from books.models import Book
 from django.core.mail import send_mail
 from django.http import HttpResponseRedirect
+from books.forms import ContactForm
 # Create your views here.
 # def search_form(request):
 # 	return render(request, 'search_form.html')
@@ -21,25 +22,17 @@ def search(request):
 	return render(request, 'search_form.html', {'errors': errors})
 
 def contact(request):
-	errors = []
-	if request.method =="POST":
-		if not request.POST.get('subject', ''):
-			errors.append('Enter a subject.')
-		if not request.POST.get('message', ''):
-			errors.append('Enter a message.')
-		if request.POST.get('email') and '@' not in request.POST['email']:
-			errors.append('Enter a valid e-mail address.')
-		if not errors:
+	if request.method == 'POST':
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			cd = form.cleaned_data
 			send_mail(
-				request.POST['subject'],
-				request.POST['message'],
-				request.POST.get('email', 'noreply@example.com'),
-				['siteowner@example.com']
+				cd['subject'],
+				cd['message'],
+				cd.get('email', 'noreply@example.com'),
+				['siteowner@example.com'],
 			)
-			return HttpResponseRedirect('/contact/thanks/')
-	return render(request, 'contact_form.html', {
-		'errors': errors,
-		'subject': request.POST.get('subject', ''),
-		'message': request.POST.get('message', ''),
-		'email': request.POST.get('email', ''),
-		})
+			return HttpResponseRedirect('/contact/thanks')
+	else:
+		form = ContactForm()
+	return render(request, 'contact_form.html', {'form': form})
